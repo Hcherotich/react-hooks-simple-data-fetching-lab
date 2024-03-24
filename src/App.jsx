077e6ffe19f70cad9f77ef9ec4012aa995ed4e33
cherotich <cherohellen14@gmail.com>
2024-03-24 00:38:01 -0700

    fetch

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+function App(){
+    const [dogImage,setDogImage] =useState(null);
+    const[isLoading,setIsLoading] =useState(true);
+}
+useEffect(() =>{
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data =>{
+        setDogImage(data.message);
+        setIsLoading(false)
+    })
+.catch(error=>{
+    console.error('Error fetching data:',error);
+    setIsLoading(false);
+});
+
+
+},[]);
+return (
+    <div>
+        {isLoading ? (
+            <p>Loading....</p>
+        ) :(
+            <img src={dogImage} alt="A Random Dog" />
+        )}
+    </div>
+);
+        
+export default App;
